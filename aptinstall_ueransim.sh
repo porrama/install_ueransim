158ddbbf0b4515ceca4b4a7aca7ef99af26ed96e
Porrama Rakchaithanakorn <porrama.pee@gmail.com>
2022-01-11 17:30:17 +0700

    Add files via upload

---
+++ aptinstall_ueransim.sh
@@ -0,0 +1,9 @@
+# Apt Install
+apt update
+apt install -y make
+apt install -y gcc
+apt install -y g++
+apt install -y libsctp-dev lksctp-tools
+apt install -y iproute2
+snap install cmake --classic
+apt update
